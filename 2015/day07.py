2e94aec54e71a5d7de9a7867f587d93a5e28f5f8
harry W <uyharry71@gmail.com>
2025-09-24 18:13:35 -0700

    completed day 7

--- 2015/day07.py
+++ 2015/day07.py
@@ -1,40 +1,55 @@
 from python_tools import *
 
-def part1(data: list[str]):
-    hm = {}
-    for instrs in data:
-        instr = instrs.split()
-        if instr[0].isdigit():  # 123 -> x
-            val = instr[0]
-            dst = instr[-1]
-            hm[dst] = val
-        elif instr[0] == "NOT": # NOT lk -> ll
-            src = instr[1]
-            dst = instr[-1]
-            hm[dst] = ~src & 0xFFFF
-        elif instr[1] == "AND": # af AND ah -> ai
-            wire1 = instr[0]
-            wire2 = instr[2]
-            dst = instr[-1]
-            hm[dst] = wire1 & wire2 
-        elif instr[1] == "OR": # af OR ah -> ai
-            wire1 = instr[0]
-            wire2 = instr[2]
-            dst = instr[-1]
-            hm[dst] = wire1 | wire2 
-        elif instr[1] == "LSHIFT": # af LSHIFT 2 -> ai
-            wire1 = instr[0]
-            n = instr[2]
-            dst = instr[-1]
-            hm[dst] = wire1 << wire2 
-        elif instr[1] == "RSHIFT": # af RSHIFT 2 -> ai
-            wire1 = instr[0]
-            n = instr[2]
-            dst = instr[-1]
-            hm[dst] = wire1 >> wire2 
-            
-    return hm[a]
+rules = {}
+cache = {}
+
+# populate the rules hashmap
+def parse(data: list[str]):
+    for line in data:
+        expr, target = line.split(" -> ")
+        rules[target] = expr.split()
+
+def evaluate(wire: str) -> int:
+    # if wire is just a number
+    if wire.isdigit():
+        return int(wire)
+    
+    # check if wire in cache
+    if wire in cache:
+        return cache[wire]
+    
+    # get the expression from the rules
+    expr = rules[wire]
+
+    if len(expr) == 1:
+        value = evaluate(expr[0])
+    elif "AND" in expr:
+        value = evaluate(expr[0]) & evaluate(expr[2])
+    elif "OR" in expr:
+        value = evaluate(expr[0]) | evaluate(expr[2])
+    elif "LSHIFT" in expr:
+        value = evaluate(expr[0]) << int(evaluate(expr[2]))
+    elif "RSHIFT" in expr:
+        value = evaluate(expr[0]) >> int(evaluate(expr[2]))
+    elif "NOT" in expr:
+        value = ~evaluate(expr[1]) & 0xFFFF
+
+    cache[wire] = value
+    return value
+    
+
+def part1(data: list[str]) -> int:
+    parse(data)
+    return evaluate("a")
+
+def part2(data: list[str]) -> int:
+    parse(data)
+    cache.clear()
+    # overwrite b with the value you found in Part 1 (example: 956)
+    rules["b"] = [str(956)]
+    return evaluate("a")
 
 if __name__ == "__main__":
     data = read_lines("day07-input.txt")
     print(f"Part1: {part1(data)}")
+    print(f"Part1: {part2(data)}")
